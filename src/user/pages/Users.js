d3def6ca0cb1f15e27bbe8aea47786ea72421fac
jenobpj <jenobgit@gmail.com>
2022-01-11 15:13:39 -0500

    add fake data

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,15 @@
 import React from "react";
 import UserList from "../components/UserList";
 const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "jenob",
+      image:
+        "https://images.pexels.com/photos/9693635/pexels-photo-9693635.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 3,
+    },
+  ];
   return <UserList />;
 };
 export default Users;
