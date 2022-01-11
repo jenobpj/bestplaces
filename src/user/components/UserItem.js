998683fe0235ada86f780a6ef7ba32b8f3b56d64
jenobpj <jenobgit@gmail.com>
2022-01-11 17:28:59 -0500

    add link to more info about the user's place

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,18 +1,21 @@
 import "./UserItem.css";
+import { Link } from "react-router-dom";
 import Avatar from "../../shared/Avatar";
 const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
-          </h3>
-        </div>
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
