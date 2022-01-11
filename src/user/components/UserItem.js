17c16ce9db1c8b174a23d2d0d9ba97e62dfe6510
jenobpj <jenobgit@gmail.com>
2022-01-11 17:19:42 -0500

    add the avatae component in UserItem

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,10 +1,11 @@
 import "./UserItem.css";
+import Avatar from "../../shared/Avatar";
 const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar image={props.image} alt={props.name} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
