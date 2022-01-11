5e246d4adaed2ca14fd8000c3ab118528026dfd4
jenobpj <jenobgit@gmail.com>
2022-01-11 14:17:10 -0500

    add rotuing to pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Newplaces from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<Newplaces />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Newplaces = () => {
+  return <h2>Newpalces</h2>;
+};
+export default Newplaces;
