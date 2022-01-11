09d4ac44c88f0842ccfd6934a60a0681b1754905
jenobpj <jenobgit@gmail.com>
2022-01-11 15:00:01 -0500

    pass the props to Useritem

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,3 +1,25 @@
+import UserItem from "./UserItem";
 import "./UserList.css";
-const UserList = (props) => {};
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No Users </h2>
+      </div>
+    );
+  }
+  return (
+    <ul>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.place}
+        />
+      ))}
+    </ul>
+  );
+};
 export default UserList;
