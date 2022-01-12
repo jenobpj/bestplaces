df9ee6b7866f01d4fe7d78e55903c8d25b7e032a
jenobpj <jenobgit@gmail.com>
2022-01-11 19:00:19 -0500

    wrap the components inside header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Newplaces from "./places/pages/NewPlace";
+import MainNavigation from "./shared/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/places/new" element={<Newplaces />} />
-        </Routes>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/places/new" element={<Newplaces />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -1,16 +1,18 @@
 import MainHeader from "./MainHeader";
+import "./MainNavigation.css";
 import { Link } from "react-router-dom";
 const MainNavigation = (props) => {
   return (
     <MainHeader>
-      <button className="main-navigation__menu-btn">
+      <button className="main-navigation__menu-btn ">
         <span />
         <span />
         <span />
       </button>
-      <h1 className="main-naviagation__title">
-        <Link to="/"> Your Places </Link>
+      <h1 className="main-navigation__title">
+        <Link to="/"> YourPlaces </Link>
       </h1>
     </MainHeader>
   );
 };
+export default MainNavigation;
