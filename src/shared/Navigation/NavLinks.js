0f2ad949f6a84a837c9ef6655fa11c6cff617d19
jenobpj <jenobgit@gmail.com>
2022-01-11 19:34:57 -0500

    add navLinks

---
+++ src/shared/Navigation/NavLinks.js
@@ -0,0 +1,21 @@
+import "NavLinks.css";
+import { NavLink } from "react-router-dom";
+const NavLinks = (props) => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">ALL USERS</NavLink>
+      </li>
+      <li>
+        <NavLink to="/U1/places">MY PLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">ADD PLACES</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">AUTHENTICATION</NavLink>
+      </li>
+    </ul>
+  );
+};
+export default NavLinks;
