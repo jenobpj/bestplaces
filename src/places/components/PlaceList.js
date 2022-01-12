24f687eedaece6425d228faf98eae9532bc7686b
jenobpj <jenobgit@gmail.com>
2022-01-12 00:15:25 -0500

    pass props to PlaceList.js

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,3 +1,32 @@
+import Card from "../../shared/Card";
+import PlaceItem from "./PlaceItem";
 import "./PlaceList.css";
-PlaceList = (props) => {};
+PlaceList = (props) => {
+  if (props.item.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found.Maybe create one ?</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <ul className="place-list">
+      {props.item.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
 export default PlaceList;
