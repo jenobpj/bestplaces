755d479c26fb472986ad615fd20a692c7d8a51c4
jenobpj <jenobgit@gmail.com>
2022-01-11 20:47:36 -0500

    Create portal to render the sidebar

--- src/shared/Navigation/SideDrawer.js
+++ src/shared/Navigation/SideDrawer.js
@@ -1,5 +1,7 @@
 import "./SideDrawer.css";
+import ReactDOM from "react-dom";
 const SideDrawer = (props) => {
-  return <aside className="side-drawer">{props.children}</aside>;
+  const content = <aside className="side-drawer">{props.children}</aside>;
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 export default SideDrawer;
