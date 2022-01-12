4c1daeb5aa93694e4cf030b7c59b8750d7b35666
jenobpj <jenobgit@gmail.com>
2022-01-11 20:53:07 -0500

    add state for managing side tab

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -1,16 +1,20 @@
 import MainHeader from "./MainHeader";
+import { useState } from "react";
 import "./MainNavigation.css";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setdrawerIsOpen] = useState(false);
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
       <MainHeader>
         <button className="main-navigation__menu-btn ">
           <span />
