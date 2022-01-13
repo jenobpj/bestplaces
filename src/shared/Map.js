1d1774d4e8a4522542901204ba4b254ddcdb46a4
jenobpj <jenobgit@gmail.com>
2022-01-13 16:22:24 -0500

    add map to the dom

--- src/shared/Map.js
+++ src/shared/Map.js
@@ -1,4 +1,23 @@
+import { useRef } from "react";
+
+import "./Map.css";
+
 const Map = (props) => {
-  return <div className={`map ${props.className}`} style={props.style}></div>;
+  const mapRef = useRef();
+
+  const map = new window.google.maps.Map(mapRef.current, {
+    center: props.center,
+    zoom: props.zoom,
+  });
+
+  new window.google.maps.Marker({ position: props.center, map: map });
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
 };
 export default Map;
